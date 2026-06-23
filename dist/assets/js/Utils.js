a81ab43430687a3d692162eb850180cec353c59c
Peter <peter86tsai@gmail.com>
2026-06-24 01:26:04 +0800

    製作header, banner, countdown, about，countdown樣式待修復

--- dist/assets/js/Utils.js
+++ dist/assets/js/Utils.js
@@ -9,4 +9,52 @@ const fetchApi = async (root) => {
     }
 };
 
-export { fetchApi };
+const countDown = (month, day, hour, min, sec) => {
+    const initEnd = () => {
+        let end = new Date();
+        end.setMonth(month - 1);
+        end.setDate(day);
+        end.setHours(hour);
+        end.setMinutes(min);
+        end.setSeconds(sec);
+
+        return end;
+    };
+
+    const calc = (end) => {
+        let current = new Date().getTime();
+        let diff = (end - current) / 1000;
+
+        let units = {
+            days: 60 * 60 * 24,
+            hours: 60 * 24,
+            min: 60,
+        };
+
+        let days = Math.floor(diff / units.days);
+
+        diff = diff % units.days;
+        let hours = Math.floor(diff / units.hours);
+
+        diff = diff % units.hours;
+        let min = Math.floor(diff / units.min);
+
+        let sec = Math.floor(diff % units.min);
+
+        let counts = {
+            days: days < 10 ? "0" + days : days,
+            hours: hours < 10 ? "0" + hours : hours,
+            minutes: min < 10 ? "0" + min : min,
+            seconds: sec < 10 ? "0" + sec : sec,
+        };
+
+        return counts;
+    };
+
+    let end = initEnd();
+    let output = calc(end);
+
+    return output;
+};
+
+export { fetchApi, countDown };
--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,5 +1,5 @@
 const { createApp } = Vue;
-import { fetchApi } from "./Utils.js";
+import { fetchApi, countDown } from "./Utils.js";
 
 const headOptions = {
     data() {
@@ -27,3 +27,35 @@ const headOptions = {
 
 const headApp = createApp(headOptions);
 headApp.mount("#head-app");
+
+const mainOptions = {
+    data() {
+        return {
+            counts: {
+                days: "0",
+                hours: "0",
+                minutes: "0",
+                seconds: "0",
+            },
+            abouts: [],
+        };
+    },
+    methods: {
+        countTime() {
+            let time = countDown(10, 31, 18, 0, 0);
+            this.counts.days = time.days;
+            this.counts.hours = time.hours;
+            this.counts.minutes = time.minutes;
+            this.counts.seconds = time.seconds;
+        },
+    },
+    async mounted() {
+        // setInterval(() => {
+        //     this.countTime();
+        // }, 1000);
+        this.abouts = await fetchApi("./assets/database/about_color.json");
+    },
+};
+
+const mainApp = createApp(mainOptions);
+mainApp.mount("#main-app");
